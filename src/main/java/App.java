291eb1fdae23c5bfced86fa95c04dfee35a3f6df
Frank Uzoka <Frankuzoka@hotmail.com>
2020-05-05 01:24:49 -0400

    Adding logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,7 +11,7 @@ public class App {
 
     static void logic(){
 
-        System.out.println("I have chosen a number between 1 and 10.  Try to guess it.\n");
+        System.out.println("I have chosen a number between 1 and 10.  Try to guess it.");
 
         System.out.print("Your guess: ");
 
